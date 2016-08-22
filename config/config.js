f04e424d68e426fa17a39853d98f5f0aac9b6a23
Gabriel Courtemanche <gabriel.courtemanche@gmail.com>
2016-08-22 10:09:27 -0400

    New certificat

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ var config = {};
 var fs = require('fs');
 
 config.ssloptions = {
-  ca: fs.readFileSync('/etc/ssl.ageg/ageg.ca.cabundle.crt'),
-  key: fs.readFileSync('/etc/ssl.ageg/ageg.ca.key'),
-  cert: fs.readFileSync('/etc/ssl.ageg/ageg.ca.crt'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/0001_chain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/sheppard.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/0001_cert.pem'),
   ciphers: 'EECDH EDH AESGCM HIGH !aNULL !eNULL !3DES !DES !DSS !EXP !IDEA !LOW !MD5 !PSK !RC4 !SRP',
   honorCipherOrder: true
 };
